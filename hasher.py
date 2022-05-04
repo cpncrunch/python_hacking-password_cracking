52c063be42ca5a625431e98dcdc98c1acbd3d34d
cpncrunch <isaacprivett@gmail.com>
2022-05-04 02:48:59 +0000

    Create hasher.py

---
+++ hasher.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+#Author: Isaac Privett
+#Date: 05-02-2022
+#Description: Simple program to has a string
+
+import hashlib
+
+hashvalue = input("Please enter a string to hash: ")
+
+hashobj1 = hashlib.md5()
+hashobj1.update(hashvalue.encode())
+print(hashobj1.hexdigest())
+
+hashobj2 = hashlib.sha1()
+hashobj2.update(hashvalue.encode())
+print(hashobj2.hexdigest())
+
+hashobj3 = hashlib.sha224()
+hashobj3.update(hashvalue.encode())
+print(hashobj3.hexdigest())
+
+hashobj4 = hashlib.sha256()
+hashobj4.update(hashvalue.encode())
+print(hashobj4.hexdigest())
+
+hashobj5 = hashlib.sha512()
+hashobj5.update(hashvalue.encode())
+print(hashobj5.hexdigest())
